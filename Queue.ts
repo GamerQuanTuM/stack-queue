b44cbd31114bbf9a5b3685a129d7af7f2ee4572b
GamerQuanTuM <shuvamsantra10@gmail.com>
2024-06-04 10:22:54 +0530

    Stack and queue simple implementation

---
+++ Queue.ts
@@ -0,0 +1,34 @@
+class Queue<T> {
+    private items: T[];
+  
+    constructor() {
+      this.items = [];
+    }
+  
+    enqueue(element: T): void {
+      this.items.push(element);
+    }
+  
+    dequeue(): T | undefined {
+      return this.items.shift();
+    }
+  
+    peek(): T | undefined {
+      return this.items[0];
+    }
+  
+    isEmpty(): boolean {
+      return this.items.length === 0;
+    }
+  
+    size(): number {
+      return this.items.length;
+    }
+  
+    clear(): void {
+      this.items = [];
+    }
+  }
+  
+
+  
